ce4481eab8a23c3cd0f0bd8e6035f6e36470f86c
Santeri Salonen <santerisa.salonen@gmail.com>
2021-11-05 15:42:57 +0200

    broken tests

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -66,7 +66,7 @@ class TestVarasto(unittest.TestCase):
 
         saatu_maara = self.varasto.ota_varastosta(2)
 
-        self.assertAlmostEqual(saatu_maara, 2)
+        self.assertAlmostEqual(saatu_maara, 112)
 
     def test_ottaminen_lisaa_tilaa(self):
         self.varasto.lisaa_varastoon(8)
